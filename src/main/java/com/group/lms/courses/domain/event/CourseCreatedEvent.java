c52d2ee2ae45708b8f8d207c7815ea940e8741c4
Alex Vega <axvg@users.noreply.github.com>
2026-01-14 18:49:16 -0500

    courseCreated event

---
+++ src/main/java/com/group/lms/courses/domain/event/CourseCreatedEvent.java
@@ -0,0 +1,19 @@
+package com.group.lms.courses.domain.event;
+
+import com.group.lms.shared.domain.event.DomainEvent;
+
+import lombok.Getter;
+
+@Getter
+public class CourseCreatedEvent extends DomainEvent {
+    private final String courseId;
+    private final String title;
+    private final String instructor;
+    
+    public CourseCreatedEvent(String courseId, String title, String instructor) {
+        super();
+        this.courseId = courseId;
+        this.title = title;
+        this.instructor = instructor;
+    }
+}

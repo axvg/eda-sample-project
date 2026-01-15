201765252a6bba4501d152e911c907205fb69b88
Alex Vega <axvg@users.noreply.github.com>
2026-01-14 21:45:14 -0500

    create event when publish course

---
+++ src/main/java/com/group/lms/courses/domain/event/CoursePublishedEvent.java
@@ -0,0 +1,20 @@
+package com.group.lms.courses.domain.event;
+
+import com.group.lms.shared.domain.event.DomainEvent;
+
+import lombok.Getter;
+
+@Getter
+public class CoursePublishedEvent extends DomainEvent {
+    private final String courseId;
+    private final String title;
+    private final double price;
+
+    public CoursePublishedEvent(String courseId, String title, double price) {
+        super();
+        this.courseId = courseId;
+        this.title = title;
+        this.price = price;
+    }
+
+}

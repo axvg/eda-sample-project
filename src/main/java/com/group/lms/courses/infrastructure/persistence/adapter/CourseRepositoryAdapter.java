23a2cbeda240c78c504e320aa56e2489b4a41140
Alex Vega <axvg@users.noreply.github.com>
2026-01-14 21:27:47 -0500

    add adapter

---
+++ src/main/java/com/group/lms/courses/infrastructure/persistence/adapter/CourseRepositoryAdapter.java
@@ -0,0 +1,20 @@
+package com.group.lms.courses.infrastructure.persistence.adapter;
+
+import org.springframework.stereotype.Component;
+
+import com.group.lms.courses.domain.model.Course;
+import com.group.lms.courses.domain.repository.CourseRepository;
+import com.group.lms.courses.infrastructure.persistence.repository.JpaCourseRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
+public class CourseRepositoryAdapter implements CourseRepository{
+    private final JpaCourseRepository jpaCourseRepository;
+
+    @Override
+    public Course save(Course course) {
+        return jpaCourseRepository.save(course);
+    }
+}

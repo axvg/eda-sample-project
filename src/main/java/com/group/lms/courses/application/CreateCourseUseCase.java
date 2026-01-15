1547f352c2b77d668b9515f6e2a2e58069d49cf7
Alex Vega <axvg@users.noreply.github.com>
2026-01-14 21:30:14 -0500

    update name

--- src/main/java/com/group/lms/courses/application/CreateCourseUseCase.java
+++ src/main/java/com/group/lms/courses/application/CreateCourseUseCase.java
@@ -15,7 +15,7 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @Service
 @RequiredArgsConstructor
-public class CourseUseCase {
+public class CreateCourseUseCase {
     private final CourseRepository repository;
     private final EventPublisher eventPublisher;
 

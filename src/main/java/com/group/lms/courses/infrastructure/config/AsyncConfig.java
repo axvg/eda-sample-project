e18effe0f7afebaec34e6bc032dfc26dcbd66cab
Alex Vega <axvg@users.noreply.github.com>
2026-01-16 20:08:15 -0500

    add async config

---
+++ src/main/java/com/group/lms/courses/infrastructure/config/AsyncConfig.java
@@ -0,0 +1,23 @@
+package com.group.lms.courses.infrastructure.config;
+
+import java.util.concurrent.Executor;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+@Configuration
+@EnableAsync // used for async event handling
+public class AsyncConfig {
+    @Bean(name = "eventExecutor")
+    public Executor asyncExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(3);
+        executor.setMaxPoolSize(5);
+        executor.setQueueCapacity(100);
+        executor.setThreadNamePrefix("event-");
+        executor.initialize();
+        return executor;
+    }
+}

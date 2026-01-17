9a67ce583b2700baf1855104b873c1d19d401db8
Alex Vega <axvg@users.noreply.github.com>
2026-01-16 20:33:38 -0500

    add retry config

---
+++ src/main/java/com/group/lms/shared/infrastructure/config/RetryConfig.java
@@ -0,0 +1,11 @@
+package com.group.lms.shared.infrastructure.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.retry.annotation.EnableRetry;
+
+
+@Configuration
+@EnableRetry
+public class RetryConfig {
+    
+}

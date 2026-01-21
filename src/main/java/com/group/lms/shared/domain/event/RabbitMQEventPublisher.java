e78920950213fe87f528d5d7d1d1b5cb34da04e9
Alex Vega <axvg@users.noreply.github.com>
2026-01-21 18:22:24 -0500

    add rabbit publisher

---
+++ src/main/java/com/group/lms/shared/domain/event/RabbitMQEventPublisher.java
@@ -0,0 +1,29 @@
+package com.group.lms.shared.domain.event;
+
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.stereotype.Component;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+import static com.group.lms.shared.infrastructure.config.RabbitMQConfig.EXCHANGE_NAME;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class RabbitMQEventPublisher {
+    private final RabbitTemplate template;
+
+    public void publish(DomainEvent event, String routingKey) {
+        log.info("Publishing event to RabbitMQ: {} - {} - {}",
+            event.getEventId(),
+            event.getEventType(),
+            event.getOcurredOn()
+        );
+        template.convertAndSend(
+            EXCHANGE_NAME,
+            routingKey,
+            event
+        );
+    }
+}

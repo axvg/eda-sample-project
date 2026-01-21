c1691c910aa34da0b2b745cb9dfb242bef450601
Alex Vega <axvg@users.noreply.github.com>
2026-01-21 18:25:38 -0500

    use rabbit publisher in usecase

--- src/main/java/com/group/lms/courses/application/CreateCourseUseCase.java
+++ src/main/java/com/group/lms/courses/application/CreateCourseUseCase.java
@@ -1,11 +1,14 @@
 package com.group.lms.courses.application;
 
+import static com.group.lms.shared.infrastructure.config.RabbitMQConfig.COURSE_CREATED_RK;
+
 import java.time.LocalDateTime;
 
 import com.group.lms.courses.domain.event.CourseCreatedEvent;
 import com.group.lms.courses.domain.model.Course;
 import com.group.lms.courses.domain.repository.CourseRepository;
 import com.group.lms.shared.domain.event.EventPublisher;
+import com.group.lms.shared.domain.event.RabbitMQEventPublisher;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -14,7 +17,8 @@ import lombok.extern.slf4j.Slf4j;
 @RequiredArgsConstructor
 public class CreateCourseUseCase {
     private final CourseRepository repository;
-    private final EventPublisher eventPublisher;
+    // private final EventPublisher eventPublisher;
+    private final RabbitMQEventPublisher eventPublisher;
 
     public Course createCourse(String title, String description, String instructor) {
         Course course = Course.builder()
@@ -29,6 +33,7 @@ public class CreateCourseUseCase {
         log.info("Course created: {} - {} - {}", saved.getId(), saved.getTitle(), saved.getInstructor());
 
         eventPublisher.publish(
+            COURSE_CREATED_RK,
             new CourseCreatedEvent(saved.getId().toString(), saved.getTitle(), saved.getInstructor())
         );
 
